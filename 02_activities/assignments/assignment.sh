4f59ca8dbeea97bb3f3568c69264263edd6de1b6
tutoringjedi <tutoringjedi@gmail.com>
2026-04-10 15:54:08 -0400

    Suppress ipaddr error in user_logs

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -41,22 +41,13 @@ mkdir -p data/processed/server_logs data/processed/user_logs data/processed/even
 # 5. Copy all server log files
 cp data/raw/*server*.log data/processed/server_logs
 
-<<<<<<< HEAD
 # 6. Repeat for user and event logs
-cp data/raw/*user*.log data/processed/user_logs
+cp data/raw/*user_log*.log data/processed/user_logs
 cp data/raw/*event*.log data/processed/event_logs
-=======
-# 6. Repeat the above step for user logs and event logs
-
-# 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rf -rf ./data
-
-# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
->>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
 
 # 7. Remove files with ipaddr in the name
 rm data/raw/*ipaddr*
-rm data/processed/user_logs/*ipaddr*
+rm data/processed/user_logs/*ipaddr* 2>/dev/null || true
 
 # 8. Create inventory.txt
 find data/processed -type f > data/inventory.txt
